ac4354f7c7dee5908f178a07d6ad8af597f901d9
Raksha25 <guptaraksha712@gmail.com>
2020-05-17 03:26:20 +0530

    Raksha added milestone(2-Darray)

---
+++ Raksha/milestone(2-Darray)/LargestSquare.java
@@ -0,0 +1,15 @@
+package twoDarray;
+
+import java.util.Scanner;
+
+public class LargestSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int n= s.nextInt();
+
+
+	}
+
+}
---
+++ Raksha/milestone(2-Darray)/monkAndInversions.java
@@ -0,0 +1,25 @@
+package twoDarray;
+
+import java.util.Scanner;
+
+public class monkAndInversions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i= 0;i <t ; i++)
+		{
+			int n = sc.nextInt();
+			int[][] arr = new int[n][n];
+			for( int j =0 ; j< n; j++)
+				for( int k =0 ; k< n; k++)
+					arr[j][k] = sc.nextInt();
+			}
+		for( int j =0 ; j< n; j++)
+			for( int k =0 ; k< n; k++)
+				
+	
+	}
+
+}

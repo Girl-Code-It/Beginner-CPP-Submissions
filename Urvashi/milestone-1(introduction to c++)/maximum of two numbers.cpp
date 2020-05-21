510b2ee08447dec6349861d705c29bbb3e50b456
urvashi-code1255 <urvashianand0906@gmail.com>
2020-05-21 15:58:55 +0530

    renamed

--- Urvashi/milestone-1(introduction to c++)/maximum of two numbers.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int a,b;
-	cin>>a>>b;
-	(a>b)?cout<<a:cout<<b;
-}
--- Urvashi/milestone-2(basics)/net bonus amount.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-float years, sal;
-cout<<"enter  your salary and years of service"<<endl;
-cin>>sal>> years;
-(years >5)?cout<<"you will get a bonus of 5% and ur net bonus amount is"<<(float)(.05*sal):cout<<"u will not get any bonus";	
-}

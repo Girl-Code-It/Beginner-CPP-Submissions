49fabeffc0f9ba2cfa66735e5e517da1857a5474
Rayman-Sodhi <65675692+Rayman-Sodhi@users.noreply.github.com>
2020-05-22 15:09:01 +0530

    Update functions for grades.cpp

--- Rayman/milestone 10/functions for grades.cpp
+++ Rayman/milestone 10/functions for grades.cpp
@@ -1,14 +1,4 @@
-/*
-Write a program which will ask the user to enter his/her marks (out of 100). Define a function that will display grades according to the marks entered as below:
-Marks        Grade
-91-100         AA
-81-90          AB
-71-80          BB
-61-70          BC
-51-60          CD
-41-50          DD
-<=40          Fail
-*/
+
 #include <iostream>
 using namespace std;
 const char* grade(int marks);

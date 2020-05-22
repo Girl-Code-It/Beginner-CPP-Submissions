fd72ac9b1d13059361f54e096b9e11a1c73052bd
Rayman-Sodhi <65675692+Rayman-Sodhi@users.noreply.github.com>
2020-05-22 15:10:00 +0530

    Update 2. place of service.cpp

--- Rayman/milestone 2/2. place of service.cpp
+++ Rayman/milestone 2/2. place of service.cpp
@@ -1,17 +1,5 @@
 /*
-2.Ask user to enter age, sex ( M or F ), marital status ( Y or N )
-and then using following rules print their place of service.
 
-if employee is female, then she will work only in urban areas.
-
-if employee is a male and age is in between 20 to 40 then he
-    may work in anywhere
-
-if employee is male and age is in between 40 t0 60 then he
-will work in urban areas only.
-
-And any other input of age should print "ERROR".
-*/
 #include<iostream>
 using namespace std;
 int main()

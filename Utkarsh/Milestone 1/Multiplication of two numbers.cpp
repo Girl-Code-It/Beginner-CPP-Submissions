cd4f7fe58215878ad44a9212c748d5cf1156365b
Utkarsh Thakur <56153378+utkarsh92054@users.noreply.github.com>
2020-05-28 15:19:30 +0530

    Create Multiplication of two numbers.cpp

---
+++ Utkarsh/Milestone 1/Multiplication of two numbers.cpp
@@ -0,0 +1,23 @@
+// Created by Utkarsh Thakur on 13/05/20.
+//  Copyright © 2020 Utkarsh Thakur. All rights reserved.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+//  Taking variable to store the numbers which user will enter and for in-program calculations.
+    int n1,n2;
+    
+//  I/O Statements regarding the number for a user friendly interface.
+    cout<<"MULTIPLICATION\n\n";
+    cout<<"Enter the first number: ";
+    cin>>n1;
+    cout<<"Enter the second number: ";
+    cin>>n2;
+    
+//  Calculation and result part.
+    cout<<"\n\nMultiplication is: "<<n1*n2<<"\n\n";
+    
+    return 0;
+}

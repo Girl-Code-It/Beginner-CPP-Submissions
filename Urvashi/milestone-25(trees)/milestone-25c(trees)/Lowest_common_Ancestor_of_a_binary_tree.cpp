8ed5da21a09b39b6de1d4065ddd7f46ee4e4ecaa
urvashi-code1255 <urvashianand0906@gmail.com>
2020-06-26 20:31:17 +0530

    yoo

---
+++ Urvashi/milestone-25(trees)/milestone-25c(trees)/Lowest_common_Ancestor_of_a_binary_tree.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+    if(root == NULL)
+        return NULL;
+        
+    // if left and right descendants are having value equal to root then root is LCA.
+    if(root -> val == p -> val || root -> val == q -> val) 
+        return root;
+    
+    struct TreeNode* l = lowestCommonAncestor(root -> left, p, q);
+    struct TreeNode* r = lowestCommonAncestor(root -> right, p, q);
+    
+    if(l && r)    // if both root->left and root->right have p and q then root is LCA.
+        return root;
+    else if(l)   // if root -> left has p q. find the res in the left branch
+        return l;
+    else if(r)   // if root -> right has p q. find the rest in the right branch
+        return r;
+    else 
+        return NULL;
+    }
+};

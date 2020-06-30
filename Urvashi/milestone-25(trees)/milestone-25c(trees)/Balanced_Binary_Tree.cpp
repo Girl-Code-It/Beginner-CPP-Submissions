c21c78c32836846148f28dd7fb9e4ff6cdfc83c0
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-06-30 05:41:22 +0530

    Balanced_Binary_Tree.cpp

--- Urvashi/milestone-25(trees)/milestone-25c(trees)/Balanced_Binary_Tree.cpp
+++ Urvashi/milestone-25(trees)/milestone-25c(trees)/Balanced_Binary_Tree.cpp
@@ -8,12 +8,14 @@ class Solution {
         return 1 + max(depth(root->left) , depth(root->right));
     }
     
-    bool isBalanced(TreeNode* root) {
+    bool isBalanced(TreeNode* root) 
+    {
         if(!root)
             return true;       
         
          int ld = depth(root->left);//find the depthe via left child
          int rd = depth(root->right);//find the depthe via right child
+        
          if(abs(ld-rd)>1)            //if diff is more than 1 then not balanced
             return false;
         

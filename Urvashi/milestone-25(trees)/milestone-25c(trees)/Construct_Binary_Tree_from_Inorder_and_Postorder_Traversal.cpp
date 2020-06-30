8d44bc71ecb71b9fe0bbfac571c897ab7c59980f
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-06-30 05:42:35 +0530

    Construct_Binary_Tree_from_Inorder_and_Postorder

--- Urvashi/milestone-25(trees)/milestone-25c(trees)/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
+++ Urvashi/milestone-25(trees)/milestone-25c(trees)/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp
@@ -1,7 +1,8 @@
 
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) 
+    {
         return Tree(inorder , postorder , 0 , inorder.size()-1 , 0 , postorder.size()-1);
     }
     
@@ -17,6 +18,7 @@ public:
             i++;
         
         TreeNode* t = new TreeNode(val);
+        
         t->left = Tree( inorder ,postorder, iBegin , i-1 , pBegin , pBegin+(i-1-iBegin));
         t->right = Tree(inorder, postorder , i+1 , iEnd , pEnd-(iEnd-i) , pEnd-1);
         return t;

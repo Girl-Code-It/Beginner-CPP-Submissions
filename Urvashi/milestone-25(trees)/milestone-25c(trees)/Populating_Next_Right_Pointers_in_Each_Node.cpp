70869ad0a778a2ed9fd0393bb63b9a764b7d64fb
urvashi-code1255 <urvashianand0906@gmail.com>
2020-07-01 22:22:28 +0530

    u

---
+++ Urvashi/milestone-25(trees)/milestone-25c(trees)/Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root)
+            return NULL;
+        queue<Node*>q;
+        q.push(root);
+        
+        
+        while(!q.empty())
+        {
+            int size = q.size();
+            while(size--)
+            {
+                Node* temp = q.front();
+                q.pop();
+                
+                if(temp->left)
+                {
+                    temp->left->next = temp->right;
+                    q.push(temp->left);
+                }
+                
+                if(temp->right)
+                {
+                    if(temp->next)
+                    temp->right->next = temp->next->left;
+                    q.push(temp->right);
+                }
+            }
+        }
+        return root;
+    }
+};

79855259c42628eac0385903b77e4a986b52d6af
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-07-02 22:53:10 +0530

    updated

--- Urvashi/milestone-25(trees)/milestone-25c(trees)/linked_list_in_binary_tree.cpp
+++ Urvashi/milestone-25(trees)/milestone-25c(trees)/linked_list_in_binary_tree.cpp
@@ -8,14 +8,15 @@ public:
         if(!root) // for empty tree.
             return false;
         if(root->val == head->val ) // if root node is equal to some node in linked list 
-            // then check if linked list is present in left or right subtree.
-            return dfs(head->next, root->left) || dfs(head->next, root->right) ; 
+            
+            return dfs(head->next, root->left) || dfs(head->next, root->right) ; // then check if linked list is present in left or right subtree.
         else
             return false;
         
     }
     
-    bool isSubPath(ListNode* head, TreeNode* root) {
+    bool isSubPath(ListNode* head, TreeNode* root) 
+    {
         if(!root)
             return false;
         if(!head)

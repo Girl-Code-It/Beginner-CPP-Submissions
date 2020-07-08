fa673f1dd77caac42a1abddcea6ebcbdbd679dc0
urvashi-code1255 <urvashianand0906@gmail.com>
2020-07-08 21:53:44 +0530

    milestone-26 added

---
+++ Urvashi/milestone-26(N-ary Tree)/milestone-26a/Maximum_depth_of_N-ary_tree.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    int maxDepth(Node* root) {
+        if(!root)
+            return 0 ;
+        vector<Node*>v = root->children;
+        int maxD = 0 ; 
+        for(auto i : v)
+            maxD = max(maxD , maxDepth(i));
+        return maxD + 1 ;
+}
+};
---
+++ Urvashi/milestone-26(N-ary Tree)/milestone-26a/N-ary_Tree_Level_Order_Traversal.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+         vector<vector<int>> ans;
+         if(!root)
+             return ans;
+         queue<Node*> q;
+         q.push(root);
+        
+        while (!q.empty())
+        {
+            int k=q.size();
+            vector<int>temp;
+            
+            for (int j=0;j<k;j++)
+            {
+                Node* u=q.front();
+                q.pop();
+                temp.push_back(u->val);
+                for (auto v:u->children)
+                {
+                    if(v)
+                        q.push(v);
+                }
+        }
+        ans.push_back(temp);   
+    }
+    return ans;
+    }
+};

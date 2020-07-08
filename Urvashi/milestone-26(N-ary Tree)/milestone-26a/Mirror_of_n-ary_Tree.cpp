fda6b797453c5e1a40cef0f1035bda6d6a54b28a
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-07-08 22:05:13 +0530

    Mirror_of_n-ary_Tree.cpp

--- Urvashi/milestone-26(N-ary Tree)/milestone-26a/Mirror_of_n-ary_Tree.cpp
+++ Urvashi/milestone-26(N-ary Tree)/milestone-26a/Mirror_of_n-ary_Tree.cpp
@@ -7,8 +7,7 @@ void mirrorTree(Node * root)
     // Number of children of root 
     int n = root->child.size(); 
   
-    // If number of child is less than 2 i.e. 
-    // 0 or 1 we do not need to do anything 
+    // If number of child is less than 2 i.e. 0 or 1 we do not need to do anything 
     if (n < 2) 
         return; 
   
@@ -16,7 +15,6 @@ void mirrorTree(Node * root)
     for (int i=0; i<n; i++) 
         mirrorTree(root->child[i]); 
   
-    // Reverse vector (variable sized array) of child 
-    // pointers 
+    // Reverse vector (variable sized array) of child pointers 
     reverse(root->child.begin(), root->child.end()); 
 } 

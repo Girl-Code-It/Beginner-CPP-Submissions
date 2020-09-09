090ee40274767b04ea40dc54be842399b77fe68c
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2020-09-09 10:04:53 +0530

    Add files via upload

---
+++ Amrita/MILESTONE1/milestone1Q3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1;
+    cout<<"ENTER THE NUMBER\n";
+    cin>>num1;
+    if(num1%2==0)
+        cout<<"THE  NUMBER IS EVEN ";
+    else
+        cout<<"THE NUMBER IS ODD ";
+    return 0;
+}
---
+++ Amrita/MILESTONE1/milestone1Q4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float num;
+    cout<<"ENTER THE NUMBERS TO BE COMPARED\n";
+    cin>>num;
+    if(num>0)
+        cout<<"THE  NUMBER IS POSITIVE  ";
+    else if(num<0)
+        cout<<"THE  NUMBER IS NEGATIVE  ";
+        else
+            cout<<"THE NUMBER IS 0 ";
+    return 0;
+}
+

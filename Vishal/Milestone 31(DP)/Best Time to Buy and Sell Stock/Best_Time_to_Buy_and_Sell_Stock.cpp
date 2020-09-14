77b75fd73ccec5cc468c091987ff24d1b9b9fa3f
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-09-14 18:24:48 +0530

    Update Best_Time_to_Buy_and_Sell_Stock.cpp

--- Vishal/Milestone 31(DP)/Best Time to Buy and Sell Stock/Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Vishal/Milestone 31(DP)/Best Time to Buy and Sell Stock/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -2,9 +2,12 @@
 
 /*
 Simple Solution : You will get maximum profit when you buy at minimum price and sell at maximum price.
-Condition : You have to buy first.
+Condition : You have to buy first. You cant sell before buying.
+
 So just if you want to sell on ith day then you will get maxmimum profit iff you had buy the stock at 
 minimum price from day 1 to i.
+
+Time Complexity - O(N), Space Complexity - O(1)
 */
 
 class Solution

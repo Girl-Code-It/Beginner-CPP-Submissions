ec7c7eb9b6fef70f44cea1e34ab069e0499f4bc6
Amritapandey22 <69748558+Amritapandey22@users.noreply.github.com>
2020-09-17 02:22:32 +0530

    Add files via upload

---
+++ Amrita/MILESTONE 2/Milestone2Q10.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x =2, y=5, z=0;
+    cout<<"THE OUTPUTS FOR THE GIVEN CONDITION ARE AS FOLLOWS \n  "<<"0 STANDS FOR FALSE AND 1 STANDS FOR TRUE \n";
+    cout<<(x==2)<<endl;
+     cout<<(x!=5)<<endl;
+      cout<<(x!=5 && y>=5)<<endl;
+       cout<<(z!=0 && x==2)<<endl ;
+        cout<<(!(y<10))<<endl;
+    return 0;
+}

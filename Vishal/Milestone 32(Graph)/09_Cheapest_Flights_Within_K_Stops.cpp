bf79936a5dac6f3c43d5b9072fdda20aa0990d66
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-10-06 18:25:21 +0530

    More explanation

--- Vishal/Milestone 32(Graph)/09_Cheapest_Flights_Within_K_Stops.cpp
+++ Vishal/Milestone 32(Graph)/09_Cheapest_Flights_Within_K_Stops.cpp
@@ -1,28 +1,31 @@
 // https://leetcode.com/problems/cheapest-flights-within-k-stops/
 
 /*
-    It also have multiple solution. But we will see that algorithms later....
-
     Now we do this problem using DFS + Pruining (TO Prune unneccesary Calls)
-
 */
 
 class Solution
 {
     void solve(vector<vector<pair<int, int>> >&adj, int src, int dst, int k, int &fare, int totCost, vector<int>&visited)
     {
+        // if k < -1 it implies, we have crossed k stops, Now it isn't possible to reach destination.
         if(k < -1)
         return;
 
+        // if we reach destination, then check it is the cheapest route or not.
         if (src == dst)
         {
             fare = min(fare, totCost);
             return;
         }
 
+        // If we haven't reach destination and we have some stops as well. then make current stop as visited. 
         visited[src] = true;
         for (int i = 0; i < adj[src].size(); i++)
         {
+            // We will make a dfs call iff its already cost is lower than fare.
+            // Because if it is already greater than fare, so there is no need to move make dfs call.
+            // And this is called pruining.(Cut unnecessary dfs calls)
             if (!visited[adj[src][i].first] && totCost + adj[src][i].second <= fare)
             {
                 solve(adj, adj[src][i].first, dst, k-1, fare, totCost + adj[src][i].second, visited);

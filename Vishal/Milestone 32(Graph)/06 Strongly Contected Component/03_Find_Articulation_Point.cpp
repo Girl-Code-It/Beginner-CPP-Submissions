0ff474193ac41df7eba2f1da44fca5ee2452dd2d
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-10-08 19:37:27 +0530

    Adding articulation point and Bridges problem

---
+++ Vishal/Milestone 32(Graph)/06 Strongly Contected Component/03_Find_Articulation_Point.cpp
@@ -0,0 +1,83 @@
+/* 
+    https://leetcode.com/problems/critical-connections-in-a-network/
+
+    Articulation Point : A node is called articulation point if after removing that node and its associated edges,
+    we will get more components than earlier.
+
+    We will do simple Tarjans algorithms with two extra cases, which are :
+    1. If a node is root node and it have more than one child.
+    2. If a node is not a root node and it have atleast one component which do not have any back edge to the root.
+
+    Time complexity:- O(V+E)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define V 5
+#define pb push_back
+
+unordered_map<int, vector<int>> adj;
+
+void DFS(int u, vector<int> &disc, vector<int> &low, vector<int> &parent, vector<bool> &articulation_Point)
+{
+    static int time = 0;
+    disc[u] = low[u] = time;
+    time += 1;
+    int children = 0;
+
+    for (int v : adj[u])
+    {
+        if (disc[v] == -1) 
+        {
+            children += 1;
+            parent[v] = u;
+            DFS(v, disc, low, parent, articulation_Point);
+            low[u] = min(low[u], low[v]);
+
+            //Case-1: U is root
+            if (parent[u] == -1 and children > 1) 
+                articulation_Point[u] = true;
+
+            //Case-2: Atleast 1 component will get separated
+            if (parent[u] != -1 and low[v] >= disc[u]) 
+                articulation_Point[u] = true;
+        }
+        //Ignore child to parent edge
+        else if (v != parent[u]) 
+            low[u] = min(low[u], disc[v]);
+    }
+}
+
+void findAPs_Tarjan()
+{
+    vector<int> disc(V, -1), low(V, -1), parent(V, -1);
+    vector<bool> articulation_Point(V, false); //Avoids cross-edge
+
+    for (int i = 0; i < V; ++i)
+        if (disc[i] == -1)
+            DFS(i, disc, low, parent, articulation_Point);
+
+    cout << "Articulation Points are: ";
+    for (int i = 0; i < V; ++i)
+        if (articulation_Point[i])
+            cout << i << " ";
+}
+
+int main()
+{
+    adj[0].pb(2);
+    adj[2].pb(0);
+    adj[0].pb(3);
+    adj[3].pb(0);
+    adj[1].pb(0);
+    adj[0].pb(1);
+    adj[2].pb(1);
+    //adj[2].pb(4);
+    //adj[4].pb(2);
+    adj[1].pb(2);
+    adj[3].pb(4);
+    adj[4].pb(3);
+
+    findAPs_Tarjan();
+    return 0;
+}
---
+++ Vishal/Milestone 32(Graph)/06 Strongly Contected Component/04_Find_Bridges.cpp
@@ -0,0 +1,71 @@
+/* 
+    Bridge : It is an edge which if it is removed then the graph will split into components.
+
+    Time complexity:- O(V+E)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define V 5
+#define pb push_back
+
+unordered_map<int, vector<int>> adj;
+
+void DFS(int u, vector<int> &disc, vector<int> &low, vector<int> &parent, vector<pair<int, int>> &bridge)
+{
+    static int time = 0;
+    disc[u] = low[u] = time; 
+    time++; 
+
+    for (int v : adj[u])
+    {
+        //If v is not visited
+        if (disc[v] == -1) 
+        {
+            // we will make an edge from u to v and do a DFS.
+            parent[v] = u; 
+            DFS(v, disc, low, parent, bridge); 
+
+            // Update low value of parent
+            low[u] = min(low[u], low[v]);
+
+            if (low[v] > disc[u])
+                bridge.pb({u, v});
+        }
+        //Ignore child to parent edge.
+        else if (v != parent[u]) 
+            low[u] = min(low[u], disc[v]);
+    }
+}
+
+void findBridges_Tarjan()
+{
+    vector<int> disc(V, -1), low(V, -1), parent(V, -1);
+
+    vector<pair<int, int>> bridge;
+
+    for (int i = 0; i < V; ++i)
+        if (disc[i] == -1) 
+            DFS(i, disc, low, parent, bridge); 
+    
+    cout << "Bridges are: \n"; 
+    for (auto it : bridge)
+        cout << it.first << "-->" << it.second << "\n";
+}
+
+int main()
+{
+    adj[0].pb(2);
+    adj[2].pb(0);
+    adj[0].pb(3);
+    adj[3].pb(0);
+    adj[1].pb(0);
+    adj[0].pb(1);
+    adj[2].pb(1);
+    adj[1].pb(2);
+    adj[3].pb(4);
+    adj[4].pb(3);
+
+    findBridges_Tarjan();
+    return 0;
+}

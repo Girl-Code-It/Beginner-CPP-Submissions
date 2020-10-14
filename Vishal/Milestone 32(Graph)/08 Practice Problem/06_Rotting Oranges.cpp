0583a6fba4c688d68030b53c80bc6e5fb2b249bb
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-10-14 22:16:44 +0530

    All leetcode questions are done

--- Vishal/Milestone 32(Graph)/08 Practice Problem/06_Rotting Oranges.cpp
+++ Vishal/Milestone 32(Graph)/08 Practice Problem/06_Rotting Oranges.cpp
@@ -9,8 +9,6 @@
     2. All oranges will rotted after some time.
     => So, we need a minimum time. We can find minimum time using BFS.
 
-
-    
 */
 
 class Solution
@@ -18,6 +16,61 @@ class Solution
 public:
     int orangesRotting(vector<vector<int>> &grid)
     {
+        int n = grid.size(), m = grid[0].size(), fresh = 0;
 
+        // Push rotten oranges for BFS, pair define coordinates.
+        queue<pair<int, int> >q;
+
+        // for all posible direction
+        vector<pair<int,int> >dir = {{-1,0},{1,0},{0,-1},{0,1}};
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                // Increase the count of fresh oranges
+                if(grid[i][j] == 1)
+                fresh++;
+
+                // Add rotten oranges in queue
+                if(grid[i][j] == 2)
+                q.push({i,j});
+            }
+        }
+        
+        // Initialised to -1 since after each step we increment the time by 1 and initially all rotten oranges started at 0.
+        int ans = -1;
+
+        // Doing BFS
+        while(!q.empty())
+        {
+            int size = q.size();
+            ans++;
+            while (size--)
+            {
+                auto t = q.front();
+                q.pop();
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int r = t.first + dir[i].first;
+                    int c = t.second + dir[i].second;
+
+                    if (r >= 0 && r < n && c >= 0 && c < m && grid[r][c] == 1)
+                    {
+                        // Make orange rotten, push in queue and decrease count of fresh oranges
+                        grid[r][c] = 2;
+                        q.push({r,c});
+                        fresh--;
+                    }
+                }
+            }
+        }
+
+        // If fresh oranges are there in grid then return -1
+        if(fresh)
+            return -1;
+        
+        return ans == -1 ? 0 : ans;
     }
 };

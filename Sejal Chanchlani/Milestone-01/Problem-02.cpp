46fff1a2d6af4fb70e12f9ec2eacff8e64f71f17
sejalchanchlani <69971027+sejalchanchlani@users.noreply.github.com>
2020-11-10 16:46:32 +0530

    Add files via upload

---
+++ Sejal Chanchlani/Milestone-01/Problem-02.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter first number"<<endl;
+	cin>>a;
+	cout<<"enter second number"<<endl;
+	cin>>b;
+	if(a>b)
+	{
+		cout<<a<<" is the greater one."<<endl;
+	}
+	else
+	{
+		cout<<b<<" is the greater one."<<endl;
+	}
+}

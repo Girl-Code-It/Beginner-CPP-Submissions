05b7c0deeab56ae6c838c6caf28a9d51b3c25346
sejalchanchlani <69971027+sejalchanchlani@users.noreply.github.com>
2020-11-10 17:58:53 +0530

    submitting milestone-01 (#106)
    * Create Sejal Chanchlan

---
+++ Sejal Chanchlani/Milestone-01/Problem-03.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter any number"<<endl;
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<a<<" is an even number."<<endl;
+	}
+	else
+	{
+		cout<<a<<" is an odd number";
+	}
+}
+
---
+++ Sejal Chanchlani/Milestone-01/Problem-05.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter first number"<<endl;
+	cin>>a;
+	cout<<"enter second number"<<endl;
+	cin>>b;
+	cout<<"Multiplication of "<<a<<" and "<<b<<" is "<<a*b;
+}

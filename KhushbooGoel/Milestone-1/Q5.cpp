a55c3e3af697ec4de42ab2d8b05ae8bc21e7de34
khushboog12 <khushboogoel1203@gmail.com>
2020-12-17 18:53:07 +0530

    Milestone-1

---
+++ KhushbooGoel/Milestone-1/Q5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout << "Enter two numbers = ";
+    cin >> num1 >> num2;
+    cout << "Multiplication of two nos. = " << num1*num2;
+    return 0;
+}

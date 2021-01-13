aa53f797da928d27c0fa32cafdd84f088caa51b2
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-13 23:10:59 +0530

    Add files via upload

---
+++ Gursharan Kaur/Milestone 1(Introduction To C++)/Practice/3.Even Or Odd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number:";
+    cin>>a;
+    if(a%2==0)
+        cout<<"No. is Even";
+    else
+        cout<<"No. is Odd";
+}

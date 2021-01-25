858452eb3db5fc041ac3f45840cd1803d2f7ac76
Swapna Devi <65804603+Swapna804@users.noreply.github.com>
2021-01-25 09:18:03 +0530

    milestone 2

---
+++ Swapna Devi/milestone2/4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    while(i<=100)
+    {
+       
+        if(i%2 == 0)
+        {
+           cout<<i<<" ";
+        }
+        i++;
+    }
+
+}
---
+++ Swapna Devi/milestone2/7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count,n,sum=0;
+    cout<<"Enter number";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        sum+=i;
+    }
+    cout<<"sum is"<<sum;
+    return 0;
+}

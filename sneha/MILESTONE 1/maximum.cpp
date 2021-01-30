4f0f08608e257061b1ae081e527f9ffccbf17551
Snehaa1203 <77047835+Snehaa1203@users.noreply.github.com>
2021-01-31 01:04:41 +0530

    Add files via upload

---
+++ sneha/MILESTONE 1/maximum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{    int a,b;
+    cout<<"enter 2 numbers";
+    cin>>a>>b;
+    if(a>b)
+        cout<<"max"<<a;
+    else
+        cout<<"max"<<b;
+    return 0;
+
+}
---
+++ sneha/MILESTONE 1/welcome.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Welcome To Girl Code IT";
+    return 0;
+}

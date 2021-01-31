4974f57bdfecf53370b6b5bbaba587e3afc52e84
Snehaa1203 <77047835+Snehaa1203@users.noreply.github.com>
2021-01-31 15:58:11 +0530

    Add files via upload

---
+++ sneha/MILESTONE 2/Q4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    while(i<=100)
+    {
+        if(i%2==0)
+            cout<<i<<endl;
+        i++;
+    }return 0;
+}
---
+++ sneha/MILESTONE 2/Q9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{      int n;
+       cin>>n;
+
+       for (int i=1;i<=10;i++)
+            cout<<i*n<<endl;
+            return 0;
+
+}

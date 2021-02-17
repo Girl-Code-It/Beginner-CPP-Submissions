469c34a7037660e14ca3ba6b9242fbd810d3d058
Yashika <yashikajotwani12@gmail.com>
2021-02-17 19:45:40 +0530

    milestone-1(done)

---
+++ yashika jotwani/milestone1/maximum_of_two.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter two numbers";
+    cin>>num1>>num2;
+    if(num1>num2)
+    {
+        cout<<"num1 is greater "<<num1;
+    }else{
+        cout<<"num2 is greater "<<num2;
+    }
+return 0;
+
+
+}

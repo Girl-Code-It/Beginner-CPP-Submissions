5356fe00d08765f0661857b7de1d17969f5ba13c
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2021-02-20 17:00:31 +0530

    Milestone 1->completed! (#170)

---
+++ Vedant/MILESTONE 1/Code 5.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n1;
+    cout<<"Enter first number=";
+    cin>>n1;
+    int n2;
+    cout<<"Enter second number=";
+    cin>>n2;
+    cout<<endl<<n1<<"X"<<n2<<"="<<n1*n2;
+}
